5bf508e1d495fb07f3cc52d38e6f9bdd134e5e95
Nemésio Florence Vieira Filho <54808745+NemesioFVF@users.noreply.github.com>
2021-02-02 03:18:20 -0300

    Add files via upload

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número (Inteiro): ");
+            Int32 i1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o exponencial: ");
+            Int32 i2 = Convert.ToInt32(Console.ReadLine());
+            Double r = Math.Pow(i1, i2);
+            Console.WriteLine("O resultado é: " + r);
+            Console.ReadKey();
+        }
+    }
+}
